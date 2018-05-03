6b6c7e514686432d099c6f9be7f67bbc39d57d6f
Marco Scaletta <scaletta.marco@educ.di.unito.it>
2018-05-03 22:27:25 +0200

    Added constructor and removed Tag.END use

--- src/main/java/com/treeBankReader/TreeBankReader.java
+++ src/main/java/com/treeBankReader/TreeBankReader.java
@@ -1,30 +1,30 @@
 package com.treeBankReader;
 
-import com.newTools.BiGramNew;
-import com.newTools.PoSTagNew;
-import com.newTools.TagEnum;
+import com.taggingTool.BiGram;
+import com.taggingTool.PoSTag;
+import com.taggingTool.Tag;
 import com.utilities.Log;
-import com.utilitiesIO.UtilitiesIO;
+import com.utilities.UtilitiesIO;
 import org.apache.commons.lang3.time.StopWatch;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class TreeBankReader {
-    private final HashMap<TagEnum, Long> tagNums = new HashMap<>();
+
+    private final HashMap<Tag, Long> tagNums = new HashMap<>();
     private final HashMap<String, Long> poSTagPerWordNums = new HashMap<>();
-    private final HashMap<PoSTagNew, Long> poSTagNums = new HashMap<>();
-    private final HashMap<BiGramNew, Long> bigramNums = new HashMap<>();
+    private final HashMap<PoSTag, Long> poSTagNums = new HashMap<>();
+    private final HashMap<BiGram, Long> bigramNums = new HashMap<>();
 
+    public TreeBankReader(String filePath) {
+        fileToPoSTags(filePath);
+    }
 
-
-
-    public void fileToPoSTags(String filePath, int nGramLength){
-        Log.log("starting reading file: " + filePath);
+    private void fileToPoSTags(String filePath){
+        Log.log("Reading TreeBank file <" + filePath +">");
 
         StopWatch s = StopWatch.createStarted();
         FileReader fr = null;
@@ -37,8 +37,8 @@ public class TreeBankReader {
             String currentLine;
             String[] columns;
 
-            TagEnum tag1 = null;
-            TagEnum tag2;
+            Tag tag1 = null;
+            Tag tag2;
             String word;
 
             Integer numRow;
@@ -47,48 +47,38 @@ public class TreeBankReader {
                 numRow = getIntegerString(columns[0]);
                 if(numRow != null && numRow >= 1){ // sto leggendo una riga con [NUM WORD WORD_ORIGIN TAG ...]
                     if(numRow == 1) {
-                        tag1 = TagEnum.valueOf("START");
+                        tag1 = Tag.valueOf("START");
                         saveTag(tag1);
                     }
                     try{
-                        tag2 = TagEnum.valueOf(columns[3]);
+                        tag2 = Tag.valueOf(columns[3]);
                         saveTag(tag2);
                         word = columns[1].toLowerCase();
-                        savePoSTag(new PoSTagNew(word,tag2));
+                        savePoSTag(new PoSTag(word,tag2));
 
-                        if(nGramLength == 2){
-                            saveBigram(new BiGramNew(tag1,tag2));
-                            tag1 = tag2;
-                        }
+                        saveBigram(new BiGram(tag1,tag2));
+                        tag1 = tag2;
                     }catch (IllegalArgumentException e){
                         e.printStackTrace();
                     }
-
-                }else if(columns[0].equals("")){
-                    tag2 = TagEnum.valueOf("END");
-                    saveTag(tag2);
-                    if(nGramLength == 2)
-                        saveBigram(new BiGramNew(tag1,tag2));
                 }
             }
 
-            if(nGramLength == 2)
-                saveMissingTagAndBigram();
+            saveMissingTagAndBigram();
         }catch (IOException e){
             e.printStackTrace();
         }finally {
             UtilitiesIO.closeFile(fr,br);
         }
-        Log.log("\nstop reading file: " + filePath + " time: "+ (double)s.getTime()/1000 + " seconds");
     }
 
     private void saveMissingTagAndBigram(){
-        BiGramNew newBiGram;
-        for(TagEnum tagEnum : TagEnum.values()){
-            if(!tagNums.containsKey(tagEnum))
-                tagNums.put(tagEnum, (long)1);
-            for (TagEnum tagEnum1 :TagEnum.values()){
-                newBiGram = new BiGramNew(tagEnum, tagEnum1);
+        BiGram newBiGram;
+        for(Tag tag1 : Tag.values()){
+            if(!tagNums.containsKey(tag1))
+                tagNums.put(tag1, (long)1);
+            for (Tag tag2 :Tag.values()){
+                newBiGram = new BiGram(tag1, tag2);
                 if(!bigramNums.containsKey(newBiGram))
                     bigramNums.put(newBiGram, (long)1);
             }
@@ -96,12 +86,12 @@ public class TreeBankReader {
     }
 
 
-    private void saveTag(TagEnum tag){
+    private void saveTag(Tag tag){
         Long num = tagNums.get(tag);
         tagNums.put(tag, (num == null) ? (long)1 : num+1);
     }
 
-    private void savePoSTag(PoSTagNew poSTag){
+    private void savePoSTag(PoSTag poSTag){
         Long num;
         num = poSTagPerWordNums.get(poSTag.getWord());
         poSTagPerWordNums.put(poSTag.getWord(), (num == null) ? (long)1 : num+1);
@@ -112,12 +102,12 @@ public class TreeBankReader {
 
     }
 
-    private void saveBigram(BiGramNew biGram){
+    private void saveBigram(BiGram biGram){
         Long num = bigramNums.get(biGram);
         bigramNums.put(biGram, (num == null) ? (long)1 : num+1);
     }
 
-    private Integer getIntegerString(String string) {
+    private static Integer getIntegerString(String string) {
         Integer i;
         try {
             i = Integer.parseInt(string);
@@ -128,7 +118,7 @@ public class TreeBankReader {
         return i;
     }
 
-    public HashMap<TagEnum, Long> getTagNums() {
+    public HashMap<Tag, Long> getTagNums() {
         return tagNums;
     }
 
@@ -136,11 +126,11 @@ public class TreeBankReader {
         return poSTagPerWordNums;
     }
 
-    public HashMap<PoSTagNew, Long> getPoSTagNums() {
+    public HashMap<PoSTag, Long> getPoSTagNums() {
         return poSTagNums;
     }
 
-    public HashMap<BiGramNew, Long> getBigramNums() {
+    public HashMap<BiGram, Long> getBigramNums() {
         return bigramNums;
     }
 
