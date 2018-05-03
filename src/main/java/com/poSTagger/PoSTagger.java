848b4aca107a7063320cea4ba0b8d81a6cf2ffe9
Marco Scaletta <scaletta.marco@educ.di.unito.it>
2018-05-03 22:32:42 +0200

    New version of PoSTaggers

--- src/main/java/com/poSTagger/PoSTagger.java
+++ src/main/java/com/poSTagger/PoSTagger.java
@@ -1,177 +1,67 @@
 package com.poSTagger;
 
-import com.treeBankReader.FileReaderPoSTagging;
-import com.utilities.Log;
-import com.taggingTool.Sentence;
-import com.taggingTool.Counter;
 import com.taggingTool.PoSTag;
+import com.taggingTool.Sentence;
 import com.taggingTool.Tag;
-import org.apache.commons.lang3.time.StopWatch;
+import com.treeBankReader.TreeBankReader;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
-import java.util.regex.Pattern;
 
 public class PoSTagger {
-    protected FileReaderPoSTagging training;
-    private FileReaderPoSTagging testing;
 
-    public PoSTagger(FileReaderPoSTagging training, FileReaderPoSTagging testing) {
-        this.training = training;
-        this.testing = testing;
+    private final HashMap<PoSTag, Double> poSTagGivenWordProbs = new HashMap<>();
+    protected final List<String> words= new ArrayList<>();
+
+    public PoSTagger(TreeBankReader treeBankReader) {
+        words.addAll(treeBankReader.getPoSTagPerWordNums().keySet());
+        try {
+            initProbs(treeBankReader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public Sentence poSTagging(Sentence sentenceToTag){
+    protected void initProbs(TreeBankReader treeBankReader) throws Exception {
+        HashMap<PoSTag, Long>  poSTagsNums = treeBankReader.getPoSTagNums();
+        HashMap<String, Long>  posTagPerWordNum = treeBankReader.getPoSTagPerWordNums();
+        Double d;
+        for (PoSTag poSTag : poSTagsNums.keySet()){
+            d =(double)poSTagsNums.get(poSTag)/(double)posTagPerWordNum.get(poSTag.getWord());
+            poSTagGivenWordProbs.put(poSTag, d);
+        }
+    }
+
+    public Sentence poSTagging(String [] wordsArray){
+        Sentence sentence = new Sentence();
         double maxProb;
-        Tag tag;
-        for(PoSTag sentencePT : sentenceToTag.getPoSTags()){
+        double tempProb;
+        PoSTag maxProbPT;
+        for(String s : wordsArray){
+            maxProbPT = null;
             maxProb = 0;
-            tag = new Tag("NOUN");
-            for(PoSTag trainingPT : training.getPoSTags())
-                if(sentencePT.equalsByWord(trainingPT) && maxProb < trainingPT.getProb()){
-                    //maxProb = trainingPT.getProb();
-                    maxProb = trainingPT.getProb();
-                    tag = trainingPT.getTag();
-                }
-            sentencePT.setTag(tag);
-        }
-        return sentenceToTag;
-    }
+            if(words.contains(s)){
+                for(PoSTag poSTag : poSTagGivenWordProbs.keySet()){
+                    if(s.equals(poSTag.getWord())){
 
-    public double accuracy(Sentence s) {
-        double accuracy = 0;
-        double accuracyMax = 0;
-        int num = 0;
-        int correct =0;
-        int sNum = 0;
-        if(testing.getSentences().contains(s)){
-            return 1;
-        }
-        for(Sentence sentence : testing.getSentences()){
-            if(s == null)
-                System.out.println("SENTENCE NULL");
-
-            if(Objects.requireNonNull(s).equalsByWord(sentence)){
-                for (int i = 0; i < s.getPoSTags().size(); i++) {
-
-                    if(s.getPoSTags().get(i).equals(sentence.getPoSTags().get(i)))
-                        correct++;
-                    accuracyMax = ((double)(correct))/((double)(s.getPoSTags().size()));
-                }
-            }
-
-        }
-        return accuracyMax;
-    }
-
-    private double accuracy(Sentence s, Sentence testing) {
-        int correct =0;
-        if(testing.equals(s)){
-            if(testing == s)
-                Log.log("ERRORE");
-            return 1;
-        }
-
-        if(s == null)
-            System.out.println("SENTENCE NULL");
-        if(s.equalsByWord(testing)){
-            for (int i = 0; i < s.getPoSTags().size(); i++) {
-                if(s.getPoSTags().get(i).equals(testing.getPoSTags().get(i)))
-                    correct++;
-                /* else if(s.getPoSTags().get(i).equals(new PoSTag(s.getPoSTags().get(i).getWord(), new Tag("PROPN")))){
-                    Log.log("WRONG PROPN: " + s.getPoSTags().get(i).lazyToString());
-                }else if(s.getPoSTags().get(i).equals(new PoSTag(s.getPoSTags().get(i).getWord(), new Tag("NOUN")))){
-                    Log.log("WRONG NOUN: " + s.lazyToString());
-                }*/
-            }
-        }
-        if(Double.isNaN(((double)(correct))/((double)(s.getPoSTags().size())))){
-            Log.log("NaN: correct: " + correct+", sizeList"+s.getPoSTags().size());
-        }
-
-
-        return ((double)(correct))/((double)(s.getPoSTags().size()));
-    }
-
-    public double accuracy(){
-        double temp = 0;
-        Sentence tempSentence;
-        long numSentence = testing.getSentences().size();
-        long numDone = 0;
-        long numWord = 0;
-        long totWord = 0;
-        float timeToFinish;
-        float avgTime;
-
-        for(Sentence sentence : testing.getSentences()){
-            totWord += sentence.getPoSTags().size();
-        }
-        Log.log("start accuracy ("+ this.getClass() + ")");
-        StopWatch stopWatch = StopWatch.createStarted();
-
-        final List<Double> listAccuracy = new ArrayList<>();
-        for(Sentence sentence : testing.getSentences()){
-
-            numWord += sentence.getPoSTags().size();
-
-            tempSentence = sentence.cloneSentence();
-            for(PoSTag p : tempSentence.getPoSTags())
-                p.setTag(null);
-            poSTagging(tempSentence);
-            listAccuracy.add(accuracy(tempSentence,sentence));
-            avgTime = (float)stopWatch.getTime()/(1000*numWord);
-            timeToFinish = (float)(totWord-numWord) * avgTime;
-            Log.overLog(+ ++numDone+"/"+numSentence +
-                    ", avg time for word: "+ avgTime+" seconds"+
-                    ", remain "+ (totWord-numWord) +" words"+
-                    ", finish in: "+ timeToFinish+" seconds");
-        }
-        for(Double d : listAccuracy)
-            temp+= d;
-        Log.emptyLog();
-        Log.log("LIST ACCURACY SIZE: " + listAccuracy.size());
-        Log.log("ACCURACY ON " +testing.getSentences().size() + " SENTENCES : "+(double)stopWatch.getTime()/1000+" seconds");
-        return temp/(double)testing.getSentences().size();
-    }
-
-    public double accuracyParallel(){
-        StopWatch stopWatch = StopWatch.createStarted();
-        Log.log("start accuracy ("+ this.getClass() + ")");
-        double temp = 0;
-        long numSentence = testing.getSentences().size();
-
-        Counter c = new Counter();
-        final List<Double> listAccuracy = new ArrayList<>();
-        testing.getSentences().parallelStream().forEach(sentence -> {
-                    c.incNum();
-                    Log.overLog(c.getNum()+"/"+numSentence);
-                    Sentence tempSentence1 = sentence.cloneSentence();
-                    for(PoSTag p : tempSentence1.getPoSTags())
-                        p.setTag(null);
-                    if(! (tempSentence1.getPoSTags().size() > 0))
-                        System.out.println("before postagging size == 0");
-                    poSTagging(tempSentence1);
-                    if(! (tempSentence1.getPoSTags().size() > 0))
-                        System.out.println("after postagging size == 0");
-                    double d = accuracy(tempSentence1,sentence);
-                    synchronized(this) {
-                        //int limit = 2;
-                        if(!Double.isNaN(d))
-                            listAccuracy.add(d);/*
-                            Log.log("NaN: " + sentence.lazyToString());
-                        while(!(d>0) && limit-- > 0){
-                            poSTagging(tempSentence1);
-                            d = accuracy(tempSentence1,sentence);
-                        }*/
+                        tempProb = poSTagGivenWordProbs.get(poSTag);
+                        if(tempProb > maxProb){
+                            maxProbPT = poSTag;
+                            maxProb = tempProb;
+                        }
                     }
+
                 }
-        );
-        for(Double d : listAccuracy)
-            temp+= d;
-        Log.emptyLog();
-        Log.log("ACCURACY_PARALLEL ON " +listAccuracy.size() + " SENTENCES : "+(double)stopWatch.getTime()/1000+" seconds");
-        return temp/(double)testing.getSentences().size();
+                sentence.getPoSTags().add(maxProbPT);
+            }else
+                sentence.getPoSTags().add(new PoSTag(s,Tag.PROPN));
+        }
+        return sentence;
+    }
+
+    public HashMap<PoSTag, Double> getPoSTagGivenWordProbs() {
+        return poSTagGivenWordProbs;
     }
 }
+
--- src/main/java/com/poSTagger/Viterbi.java
+++ src/main/java/com/poSTagger/Viterbi.java
@@ -1,235 +1,105 @@
 package com.poSTagger;
 
-import com.treeBankReader.FileReaderPoSTagging;
-import com.utilities.Log;
-import com.taggingTool.Sentence;
 import com.taggingTool.BiGram;
 import com.taggingTool.PoSTag;
+import com.taggingTool.Sentence;
 import com.taggingTool.Tag;
+import com.treeBankReader.TreeBankReader;
+import com.utilities.Log;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
-public class Viterbi extends PoSTagger{
+public class Viterbi extends PoSTagger {
 
-    public Viterbi(FileReaderPoSTagging training, FileReaderPoSTagging testing) {
-        super(training, testing);
+
+    private HashMap<PoSTag, Double> wordGivenPoSTagProbs;
+    private HashMap<BiGram, Double> biGramProbs;
+    private final Tag tagForMissingWordArray [];
+
+    public Viterbi(TreeBankReader treeBankReader) {
+        super(treeBankReader);
+        this.tagForMissingWordArray =  new Tag[1];
+        tagForMissingWordArray[0] = Tag.NOUN;
+
+    }
+    public Viterbi(TreeBankReader treeBankReader, Tag[] tagsForMissingWord) {
+        super(treeBankReader);
+        this.tagForMissingWordArray = tagsForMissingWord;
     }
 
     @Override
-    public Sentence poSTagging(Sentence sentence){
-        Sentence res = new Sentence();
-        List<PoSTag> poSTagList = res.getPoSTags();
+    protected void initProbs(TreeBankReader treeBankReader) throws Exception {
 
-        List<String> wordsSentence = new ArrayList();
-        for(PoSTag p : sentence.getPoSTags())
-            wordsSentence.add(p.getWord());
-        int sentenceLength = sentence.getPoSTags().size();
-        double viterbi[][] = new double[training.getNumTags()][sentenceLength+2];
-        int backpointer[][] = new int[training.getNumTags()][sentenceLength+2];
-        double newScore;
-        String newTagString;
-        viterbi[0][0]=0;
-        for (int i = 1; i < viterbi.length; i++) {
-            viterbi[i][0] = Double.NEGATIVE_INFINITY;
-            viterbi[0][viterbi[0].length-1] = Double.NEGATIVE_INFINITY;
-        }
-        for (int i = 1; i < viterbi[0].length; i++) {
-            viterbi[0][i] = Double.NEGATIVE_INFINITY;
-        }
+        wordGivenPoSTagProbs= new HashMap<>();
+        biGramProbs = new HashMap<>();
 
-        for (int t = 0; t < sentenceLength; t++) {
-            if(!training.getWords().containsKey(wordsSentence.get(t))){
-                String wordToAdd [] = {"NOUN", "VERB", "ADJ"};
-                for(String s :wordToAdd){
+        HashMap<Tag, Long>  tagNums = treeBankReader.getTagNums();
+        HashMap<PoSTag, Long>  poSTagsNums = treeBankReader.getPoSTagNums();
+        HashMap<BiGram, Long>  biGramSNums = treeBankReader.getBigramNums();
 
-                    PoSTag p = new PoSTag(wordsSentence.get(t), training.getSavedTag(s));
+        if(treeBankReader.getBigramNums().isEmpty())
+            throw new Exception("BiGrams counters is empty");
 
-                    training.saveWord(wordsSentence.get(t));
-                    training.savePoSTag(p);
-                    double posTagNum = (double) p.getNum();
-                    double tagNum = (double) p.getTag().getNum();
-                    p.setProb((posTagNum/tagNum));
-                    p.setProbFin((double)1/(double)wordToAdd.length);
-                }
-            }
+        for(BiGram biGram : biGramSNums.keySet())
+            biGramProbs.put(biGram,(double) biGramSNums.get(biGram)/(double)tagNums.get(biGram.getSndTag()));
 
-            for (int s = 0; s < training.getNumTags(); s++) {
-                //for(PoSTag p : training.getPoSTags())
+        for(PoSTag poSTag : poSTagsNums.keySet())
+            wordGivenPoSTagProbs.put(poSTag,(double) poSTagsNums.get(poSTag)/(double)tagNums.get(poSTag.getTag()));
 
-                for(BiGram biGram : training.getBiGrams()){
-                    if(biGram.getTags()[0].equals(training.getTags().get(s))) {
-                        Tag tag = biGram.getTags()[1];
-                        int indexTag = training.getTags().indexOf(tag);
-                        if (!training.getPoSTags().contains(new PoSTag(wordsSentence.get(t), tag))) {
-                            newScore = Double.NEGATIVE_INFINITY;
-                        }else {
-                            PoSTag poSTag = training.getPoSTags()
-                                    .get(training.getPoSTags().indexOf(new PoSTag(wordsSentence.get(t), tag)));
-                            //System.out.format("newScore %20s%30s%30s\n","viterbi["+s+"]["+t+"]","Math.log(biGram.getProb())", "Math.log(poSTag.getProb())");
-                            //System.out.format("newScore %20s%30s%30s\n",viterbi[s][t],Math.log(biGram.getProb()), Math.log(poSTag.getProb()));
-
-
-                            newScore = viterbi[s][t] + Math.log(biGram.getProb()) + Math.log(poSTag.getProb());
-
-                        }
-                        if(viterbi[indexTag][t+1]==0 || newScore>viterbi[indexTag][t+1]){
-                            viterbi[indexTag][t+1] = newScore;
-                            backpointer[indexTag][t+1]=s;
-                        }
-
-                    }
-                }
-
-            }
-
-        }
-        double maxProb = Double.NEGATIVE_INFINITY;
-        int indexMax = -1;
-
-        for (int i = 0; i < viterbi.length; i++) {
-            double temp = viterbi[i][viterbi[0].length-2];
-            if(temp>maxProb){
-                maxProb = temp;
-                indexMax = i;
-            }
-        }/*
-        System.out.format("%40s","");
-        for (int i = 1; i < viterbi[0].length-1; i++) {
-            System.out.format("%30s",sentence.getPoSTag(i-1).getWord());
-        }
-        System.out.println();
-        for (int i = 0; i < viterbi.length; i++) {
-            System.out.format("%10s",training.getTags().get(i).lazyToString());
-            for (int j = 0; j < viterbi[0].length; j++) {
-                System.out.format("%30s",viterbi[i][j]);
-            }
-            System.out.println();
-
-        }
-        for (int i = 0; i < backpointer.length; i++) {
-            for (int j = 0; j < backpointer[0].length; j++) {
-                System.out.format("%30s",backpointer[i][j]);
-            }
-            System.out.println();
-
-        }*/
-
-        if(maxProb > Double.NEGATIVE_INFINITY)
-            for (int j = backpointer[0].length-2 ; j > 0; j--) {
-                poSTagList.add(0, new PoSTag(wordsSentence.get(j-1), training.getTags().get(indexMax)));
-                indexMax = backpointer[indexMax][j];
-            }
-        else{
-            Log.log("maxProb < Double.NEGATIVE_INFINITY");
-            System.out.format("%40s","");
-            for (int i = 1; i < viterbi[0].length-1; i++) {
-                System.out.format("%30s",sentence.getPoSTag(i-1).getWord());
-            }
-            System.out.println();
-            for (int i = 0; i < viterbi.length; i++) {
-                System.out.format("%10s",training.getTags().get(i).lazyToString());
-                for (int j = 0; j < viterbi[0].length; j++) {
-                    System.out.format("%30s",viterbi[i][j]);
-                }
-                System.out.println();
-
-            }
-            for (int i = 0; i < backpointer.length; i++) {
-                for (int j = 0; j < backpointer[0].length; j++) {
-                    System.out.format("%30s",backpointer[i][j]);
-                }
-                System.out.println();
-
-            }
-        }
-        sentence.setPoSTags(poSTagList);
-        return sentence;
     }
 
-    public Sentence parallelPoSTagging(Sentence sentence){
-        Sentence res = new Sentence();
-        List<PoSTag> poSTagList = res.getPoSTags();
-
-        List<String> wordsSentence = new ArrayList();
-        for(PoSTag p : sentence.getPoSTags())
-            wordsSentence.add(p.getWord());
-        int sentenceLength = sentence.getPoSTags().size();
-        double viterbi[][] = new double[training.getNumTags()][sentenceLength+2];
-        int backpointer[][] = new int[training.getNumTags()][sentenceLength+2];
+    @Override
+    public Sentence poSTagging(String[] sentenceString) {
+        Sentence sentence = new Sentence();
+        double viterbi[][] = new double[Tag.values().length][sentenceString.length+2];
+        int backpointer[][] = new int[Tag.values().length][sentenceString.length+2];
         double newScore;
-        String newTagString;
-        viterbi[0][0]=0;
-        for (int i = 1; i < viterbi.length; i++) {
-            viterbi[i][0] = Double.NEGATIVE_INFINITY;
-            viterbi[0][viterbi[0].length-1] = Double.NEGATIVE_INFINITY;
-        }
-        for (int i = 1; i < viterbi[0].length; i++) {
-            viterbi[0][i] = Double.NEGATIVE_INFINITY;
-        }
+        int indexTag;
+        List<Tag> tagForMissingWord = Arrays.asList(tagForMissingWordArray);
+        PoSTag poSTag;
+        for (int i = 0; i < viterbi.length; i++) {
+            for (int j = 0; j < viterbi[0].length; j++) {
+                viterbi[i][j] = Double.NEGATIVE_INFINITY;
 
-        for (int t = 0; t < sentenceLength; t++) {
-            if(!training.getWords().containsKey(wordsSentence.get(t))){
-                /*if(t>0
-                        && !wordsSentence.get(t-1).equals(".")
-                        && !wordsSentence.get(t-1).equals("\"")
-                        && Character.isUpperCase(wordsSentence.get(t).charAt(0))){
-                    newTagString = "PROPN";
-                }else*/
-                String wordToAdd [] = {"NOUN", "VERB", "ADJ"};
-
-                newTagString = "NOUN";
-                for(String s :wordToAdd){
-                    PoSTag p = new PoSTag(wordsSentence.get(t), training.getSavedTag(s));
-                /*if(newTagString.equals("PROPN"))
-                    Log.log("NEW PROPN: " + p.lazyToString());
-                if(newTagString.equals("NOUN"))
-                    Log.log("NEW NOUN: " + p.lazyToString());*/
-                    training.saveWord(wordsSentence.get(t));
-                    if(training.getWords().get(wordsSentence.get(t)) == null)
-                        Log.log("INSERT DIDN'T WORK");
-                    else
-                        Log.log("training.getWords().get("
-                                +wordsSentence.get(t)+") == " + training.getWords().get(wordsSentence.get(t)));
-                    training.savePoSTag(p);
-                    double posTagNum = (double) p.getNum();
-                    double tagNum = (double) p.getTag().getNum();
-                    p.setProb((posTagNum/tagNum));
-                    p.setProbFin((double)1/(double)training.getWords().get(p.getWord()).size());
-                }
-
-
-                //Log.log(p.toString());
             }
+        }
+        viterbi[0][0]=0;
 
-            for (int s = 0; s < training.getNumTags()-2; s++) {
-                //for(PoSTag p : training.getPoSTags())
+        for (int t = 0; t < sentenceString.length; t++) {
+            for (int s = 0; s < Tag.values().length; s++) {
 
-                for(BiGram biGram : training.getBiGrams()){
-                    if(biGram.getTags()[0].equals(training.getTags().get(s))) {
-                        Tag tag = biGram.getTags()[1];
-                        int indexTag = training.getTags().indexOf(tag);
-                        if (!training.getPoSTags().contains(new PoSTag(wordsSentence.get(t), tag))) {
-                            newScore = Double.NEGATIVE_INFINITY;
+                for(BiGram biGram : biGramProbs.keySet()){
+                    if(biGram.getFstTag().equals(Tag.values()[s])){
+                        Tag tag = biGram.getSndTag();
+                        indexTag = Arrays.asList(Tag.values()).indexOf(tag);
+                        if (!wordGivenPoSTagProbs.keySet().contains(new PoSTag(sentenceString[t], tag))) {
+                            if (!words.contains(sentenceString[t]) && tagForMissingWord.contains(tag)) {
+                                newScore = viterbi[s][t]
+                                        + Math.log(biGramProbs.get(biGram))
+                                        + Math.log((double)1/(double)tagForMissingWord.size());
+                            }else
+                                newScore = Double.NEGATIVE_INFINITY;
                         }else {
-                            PoSTag poSTag = training.getPoSTags().get(training.getPoSTags().indexOf(new PoSTag(wordsSentence.get(t), tag)));
+                            poSTag = new PoSTag(sentenceString[t], tag);
 
-
-                            // old --> newScore = viterbi[s][t] + Math.log(biGram.getProb()) + Math.log(poSTag.getProb());
-                            newScore = viterbi[s][t] + Math.log(biGram.getProb()) + Math.log(poSTag.getProbFin());
-
+                            newScore = viterbi[s][t]
+                                    + Math.log(biGramProbs.get(biGram))
+                                    + Math.log(wordGivenPoSTagProbs.get(poSTag));
                         }
                         if(viterbi[indexTag][t+1]==0 || newScore>viterbi[indexTag][t+1]){
                             viterbi[indexTag][t+1] = newScore;
                             backpointer[indexTag][t+1]=s;
                         }
-
                     }
+
                 }
-
             }
-
         }
+
+
+
         double maxProb = Double.NEGATIVE_INFINITY;
         int indexMax = -1;
 
@@ -240,36 +110,22 @@ public class Viterbi extends PoSTagger{
                 indexMax = i;
             }
         }
-        System.out.format("%40s","");
-        for (int i = 1; i < viterbi[0].length-1; i++) {
-            System.out.format("%30s",sentence.getPoSTag(i-1).getWord());
-        }
-        System.out.println();
-        for (int i = 0; i < viterbi.length; i++) {
-            System.out.format("%10s",training.getTags().get(i).lazyToString());
-            for (int j = 0; j < viterbi[0].length; j++) {
-                System.out.format("%30s",viterbi[i][j]);
-            }
-            System.out.println();
-
-        }
-        for (int i = 0; i < backpointer.length; i++) {
-            for (int j = 0; j < backpointer[0].length; j++) {
-                System.out.format("%30s",backpointer[i][j]);
-            }
-            System.out.println();
-
-        }
 
         if(maxProb > Double.NEGATIVE_INFINITY)
-            for (int j = backpointer[0].length-2 ; j > 0; j--) {
-                poSTagList.add(0, new PoSTag(wordsSentence.get(j-1), training.getTags().get(indexMax)));
+            for (int j = backpointer[0].length-2; j > 0; j--) {
+                sentence.getPoSTags().add(0,new PoSTag(sentenceString[j-1],Tag.values()[indexMax]));
                 indexMax = backpointer[indexMax][j];
             }
-        else
-            Log.log("maxProb < Double.NEGATIVE_INFINITY");
-        sentence.setPoSTags(poSTagList);
+
         return sentence;
     }
 
+
+    public HashMap<PoSTag, Double> getWordGivenPoSTagProbs() {
+        return wordGivenPoSTagProbs;
+    }
+
+    public HashMap<BiGram, Double> getBiGramProbs() {
+        return biGramProbs;
+    }
 }
